58fa090dd6c093161504c5fff53a59a0b2092864
Pham Quang Thinh <quangthinh.pqt8386@gmail.com>
2023-10-31 23:09:24 +0700

    [Update]: Navbar

--- project-ai-saas/constants.ts
+++ project-ai-saas/constants.ts
@@ -1,4 +1,7 @@
+import { LayoutDashboard } from 'lucide-react'
+
 export const MAX_FREE_COUNTS = 5
+
 export const THEME_MODES = [
     {
         value: 'light',
@@ -7,5 +10,14 @@ export const THEME_MODES = [
     {
         value: 'dark',
         label: 'Dark'
+    }
+]
+
+export const NAVIGATIONS = [
+    {
+        title: 'Dashboard',
+        icon: '/public/Dashboard.svg',
+        url: '/dashboard',
+        slug: 'dashboard'
     }
 ]
